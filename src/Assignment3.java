3db2754f35dc7c719bb65fb117d838bb21cb7f9f
Milind <Milind@mwagh-win10>
2022-06-16 19:52:06 +0530

    method added

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -22,31 +22,28 @@ public class Assignment3 {
 		WebDriverWait w = new WebDriverWait(driver, 15);
 		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
 		System.out.println(driver.findElement(By.cssSelector("p.text-center")).getText());
-		String [] credentials = driver.findElement(By.cssSelector("p.text-center")).getText().split(" ");
+		String[] credentials = driver.findElement(By.cssSelector("p.text-center")).getText().split(" ");
 		String username = credentials[2];
-		String password = credentials[6].substring(0, credentials[6].length()-1);
+		String password = credentials[6].substring(0, credentials[6].length() - 1);
 		driver.findElement(By.id("username")).sendKeys(username);
 		driver.findElement(By.id("password")).sendKeys(password);
-		//driver.findElement(By.xpath("//label[2]//span[2]")).click();
-		//w.until(ExpectedConditions.alertIsPresent());
-		//Thread.sleep(10000);
-		//System.out.println(driver.switchTo().alert().getText());
-		//driver.switchTo().alert().accept();
+		
+		addItem(driver, w);
+	}
+
+	public static void addItem(WebDriver driver, WebDriverWait w) {
 		WebElement selectDropdown = driver.findElement(By.xpath("//select[@class='form-control']"));
 		Select dropdown = new Select(selectDropdown);
 		dropdown.selectByValue("consult");
 		driver.findElement(By.xpath("//input[@id='terms']")).click();
 		driver.findElement(By.xpath("//input[@id='signInBtn']")).click();
 		w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//app-card[1]//div[1]//div[2]//button[1]")));
-		
-		//String[] itemNeeded = {"iphone X", "Samsung Note 8", "Nokia Edge", "Blackberry"};
-		
+
 		List<WebElement> products = driver.findElements(By.cssSelector(".card-body"));
-		//List<WebElement> tital = driver.findElements(By.cssSelector(".card-title"));
-		for(int i=0; i < products.size(); i++)
-		{
-					driver.findElements(By.xpath("//div[@class='card-footer']/button")).get(i).click();
-					System.out.println("item added");
-			}
+		for (int i = 0; i < products.size(); i++) {
+			driver.findElements(By.xpath("//div[@class='card-footer']/button")).get(i).click();
+			System.out.println("item added");
 		}
 	}
+
+}
